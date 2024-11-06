0c256f6b1fd5659421d6f8e488a629ff68fd2c33
9xxng <qowop19@gmail.com>
2024-11-06 16:57:56 +0900

    nk0-20241106

---
+++ app/src/main/java/com/example/cheongyakhae/FirstActivity.kt
@@ -0,0 +1,28 @@
+package com.example.cheongyakhae
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.cheongyakhae.databinding.ActivityFirstBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class FirstActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 뷰바인딩 설정
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 3초 지연 후 MainActivity로 이동
+        lifecycleScope.launch {
+            delay(3000) // 3초 지연
+            startActivity(Intent(this@FirstActivity, MainActivity::class.java))
+            finish() // FirstActivity 종료하여 뒤로 가기 시 다시 돌아오지 않음
+        }
+    }
+}
